f834f97e6e54dbdff0a9381e8ca3d699b91968bf
Steve Chung <steve.cw.chung@gmail.com>
2022-01-30 18:20:30 -0800

    adding 99

---
+++ hashmaps/99.2sum.cpp
@@ -0,0 +1,63 @@
+/* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+  */
+ class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        /*//3cases
+        vector<int> solution;
+        //1. if matched 
+        for(auto i=nums.begin(); i<nums.end()-1;i++){
+            for(auto j=i+1; j<nums.end();j++){
+                if((*i)+(*j)==target){
+                    solution.push_back(distance(nums.begin(),i));
+                    solution.push_back(distance(nums.begin(),j));
+                    return solution;
+                }
+               
+            }
+        }
+        return solution;
+        //2. if less than target
+        
+        //3. if greater than target
+        */
+        unordered_map <int,int> m;
+        for(int i =0; i <nums.size();i++){
+            
+            if(m.find(target-nums[i])==m.end()){
+                m[nums[i]]=i;
+            }else{
+                return {m[target-nums[i]],i};
+            }
+        }
+        return {0,0};
+    }
+};
