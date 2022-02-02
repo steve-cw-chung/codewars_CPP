5b7dc62284ac4df3812d1c2023cf8ad7f628d616
Steve Chung <steve.cw.chung@gmail.com>
2022-02-01 21:46:02 -0800

    adding 107

---
+++ stringManipulations/107.reverseString.cpp
@@ -0,0 +1,36 @@
+/* Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is a printable ascii character. */
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int start = 0;
+        int end = s.size()-1;
+        
+        while(start<=end){
+            /*char temp = s[start];
+            s[start++]= s[end];
+            s[end--]= temp;
+            */
+            swap(s[start++],s[end--]);
+        }
+    }
+};
